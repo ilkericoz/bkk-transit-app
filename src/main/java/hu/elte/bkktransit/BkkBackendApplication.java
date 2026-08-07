f82c25e38d6e32d1f09164d8fdeed37ff3b4c0cf
Ilker Icoz <mustafa.i.icoz@gmail.com>
2026-08-07 03:41:43 +0200

    Initial commit: Spring Boot skeleton for BKK transit backend
    - Spring Boot project scaffold (Java, Maven wrapper)
    - Stop entity as first domain model
    - GTFS raw data and JVM crash logs excluded via .gitignore

---
+++ src/main/java/hu/elte/bkktransit/BkkBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.bkktransit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkkBackendApplication.class, args);
+	}
+
+}
